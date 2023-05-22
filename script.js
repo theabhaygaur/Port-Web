62fb7a9df4b7320ec07e5673c94cb3df04561ff5
Abhay Gaur <103801381+theabhaygaur@users.noreply.github.com>
2023-05-22 12:26:39 +0530

    script.js added

---
+++ script.js
@@ -0,0 +1,11 @@
+const video = document.getElementById('video');
+
+video.addEventListener('click', () => {
+  window.open('https://sites.google.com/view/abhayygaur', '_blank');
+});
+
+const ig = document.getElementById('ig');
+
+ig.addEventListener('click', () =>{
+    window.open('https://instagram.com/abhayygaur', '_blank');
+})
